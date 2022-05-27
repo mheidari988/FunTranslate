54f8123c3f0720c3223550e01160f8ebb03626fc
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-27 23:02:16 +0300

    Add Persistence Layer Implementations
        + Added Service Configurations
        + Added Entity Configurations
        + Added Implementation of FunTranslationRepository
        + Added Initial Migration

---
+++ FunTranslate.Persistence/Migrations/20220527194250_initial-migration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FunTranslate.Persistence.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FunTranslates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(254)", maxLength: 254, nullable: false),
+                    Translated = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    Translation = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FunTranslates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FunTranslates");
+        }
+    }
+}
---
+++ FunTranslate.Persistence/Repositories/FunTranslationRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace FunTranslate.Persistence.Repositories;
+public class FunTranslationRepository : BaseRepository<FunTranslation>, IFunTranslationRepository
+{
+    public FunTranslationRepository(FunTranslateDbContext context) : base(context) { }
+
+    public async Task<FunTranslation?> GetByTextAndTranslation(string text, string translation) =>
+        await _context.FunTranslates
+                .FirstOrDefaultAsync(fun => fun.Text == text && fun.Translation == translation);
+}

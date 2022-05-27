aaa247c298ea20908402ca87bc3421254e6a9c22
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-27 13:10:08 +0300

    Add insert to db command abstraction

---
+++ FunTranslate.Application/Feature/Persistence/FunTranslations/Commands/CreateFunTranslation/CreateFunTranslationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using FunTranslate.Application.Exceptions;
+
+namespace FunTranslate.Application.Feature.Persistence.FunTranslations.Commands.CreateFunTranslation;
+public class CreateFunTranslationCommandHandler : IRequestHandler<CreateFunTranslationCommand, Guid>
+{
+    private readonly IAsyncRepository<FunTranslation> _repository;
+    private readonly IMapper _mapper;
+
+    public CreateFunTranslationCommandHandler(IAsyncRepository<FunTranslation> repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+    public async Task<Guid> Handle(CreateFunTranslationCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new CreateFunTranslationCommandValidator();
+        var validationResult = validator.Validate(request);
+
+        if (!validationResult.IsValid)
+        {
+            throw new FunValidationException(validationResult);
+        }
+
+        var funFromDb = await _repository.AddAsync(_mapper.Map<FunTranslation>(request));
+
+        return funFromDb.Id;
+    }
+}
--- FunTranslate.Application/Usings.cs
+++ FunTranslate.Application/Usings.cs
@@ -3,3 +3,4 @@ global using MediatR;
 global using FluentValidation;
 global using FunTranslate.Domain.Entities;
 global using FunTranslate.Domain.Shared;
+global using FunTranslate.Application.Contracts.Persistence;

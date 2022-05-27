4606c6bc6e00cd15ab721d65ed141a74311d947c
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-27 13:51:33 +0300

    Add query for getting all translations from db
        + FunTranslationsListVm is a POCO but due to
            naming convention I used List word in the name
        + Query and QueryHandler currently don't have filter
            or search funtionality; better to implement down the road

---
+++ FunTranslate.Application/Feature/Persistence/FunTranslations/Queries/GetFunTranslationsList/GetFunTranslationsListQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace FunTranslate.Application.Feature.Persistence.FunTranslations.Queries.GetFunTranslationsList;
+public class GetFunTranslationsListQueryHandler : IRequestHandler<GetFunTranslationsListQuery, List<FunTranslationsListVm>>
+{
+    private readonly IAsyncRepository<FunTranslation> _repository;
+    private readonly IMapper _mapper;
+
+    public GetFunTranslationsListQueryHandler(IAsyncRepository<FunTranslation> repository, IMapper mapper)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+    public async Task<List<FunTranslationsListVm>> Handle(GetFunTranslationsListQuery request, CancellationToken cancellationToken)
+    {
+        var resultFromDb = (await _repository.GetAllAsync()).ToList();
+        return _mapper.Map<List<FunTranslationsListVm>>(resultFromDb);
+    }
+}

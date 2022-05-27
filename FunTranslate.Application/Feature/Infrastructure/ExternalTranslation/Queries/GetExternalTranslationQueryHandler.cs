a5d655f14eeede471d22db2ab0cc953eae1f702e
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-27 23:04:52 +0300

    Refactoring and updating names

--- FunTranslate.Application/Feature/Infrastructure/ExternalTranslation/Queries/GetExternalTranslationQueryHandler.cs
+++ FunTranslate.Application/Feature/Infrastructure/ExternalTranslation/Queries/GetExternalTranslationQueryHandler.cs
@@ -1,17 +1,17 @@
 ﻿using FunTranslate.Application.Contracts.Infrastructure;
 
 namespace FunTranslate.Application.Feature.Infrastructure.ExternalTranslation.Queries;
-public class GetTranslationQueryHandler : IRequestHandler<GetTranslationQuery, ExternalTranslationVm>
+public class GetExternalTranslationQueryHandler : IRequestHandler<GetExternalTranslationQuery, ExternalTranslationVm>
 {
     private readonly IExternalTranslationService _service;
     private readonly IMapper _mapper;
 
-    public GetTranslationQueryHandler(IExternalTranslationService service, IMapper mapper)
+    public GetExternalTranslationQueryHandler(IExternalTranslationService service, IMapper mapper)
     {
         _service = service;
         _mapper = mapper;
     }
-    public async Task<ExternalTranslationVm> Handle(GetTranslationQuery request, CancellationToken cancellationToken)
+    public async Task<ExternalTranslationVm> Handle(GetExternalTranslationQuery request, CancellationToken cancellationToken)
     {
         var response = await _service.TranslateAsync(request.Text, request.Translation);
         return _mapper.Map<ExternalTranslationVm>(response);

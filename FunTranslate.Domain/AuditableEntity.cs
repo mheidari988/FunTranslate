ceeb6dd54552b5bb66c7785155773bc0444cf652
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-27 12:06:47 +0300

    Add domain entities
        + AuditableEntity as base class
        + FunTranslation as an aggregate
        + FunTranslationConsts to hardcode annotation related roles.

---
+++ FunTranslate.Domain/AuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace FunTranslate.Domain;
+public class AuditableEntity
+{
+    public string CreatedBy { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public string LastModifiedBy { get; set; } = string.Empty;
+    public DateTime? LastModifiedDate { get; set; }
+}
---
+++ FunTranslate.Domain/Entities/FunTranslation.cs
@@ -0,0 +1,8 @@
+﻿namespace FunTranslate.Domain.Entities;
+public class FunTranslation : AuditableEntity
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string Translated { get; set; } = string.Empty;
+    public string Translation { get; set; } = string.Empty;
+}

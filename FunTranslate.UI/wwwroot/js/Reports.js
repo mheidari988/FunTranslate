cbe17d08ebd23b8f2536615045291bcac2e72db3
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-28 19:31:31 +0300

    Add dependency to datatables

---
+++ FunTranslate.UI/wwwroot/js/Reports.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("#reportsTable").DataTable({
+        ajax: {
+            url: 'https://localhost:7177/api/translations',
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'text' },
+            { data: 'translation' },
+            { data: 'translated' }
+        ]
+    });
+});

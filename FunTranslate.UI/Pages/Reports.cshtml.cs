604d2274406242da78c7ed4bf304b417e7b8f55b
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-28 19:32:28 +0300

    Add Reports page and wireup fetching the api

---
+++ FunTranslate.UI/Pages/Reports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FunTranslate.UI.Pages
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

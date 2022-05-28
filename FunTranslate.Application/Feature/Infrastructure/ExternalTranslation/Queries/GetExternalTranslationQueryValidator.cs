0f83eb1964511082a11ae3379cf34fd8fc32f541
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-28 23:53:37 +0300

    Add validations for GetFunTranslationByQuery

---
+++ FunTranslate.Application/Feature/Infrastructure/ExternalTranslation/Queries/GetExternalTranslationQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace FunTranslate.Application.Feature.Infrastructure.ExternalTranslation.Queries;
+public class GetExternalTranslationQueryValidator : AbstractValidator<GetExternalTranslationQuery>
+{
+    public GetExternalTranslationQueryValidator()
+    {
+        RuleFor(fun => fun.Text)
+            .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+            .NotNull()
+            .MaximumLength(FunTranslationConsts.Text.MaximumLength)
+                .WithMessage($"Maximum allowed length is {FunTranslationConsts.Text.MaximumLength}");
+
+        RuleFor(fun => fun.Translation)
+            .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+            .NotNull()
+            .MaximumLength(FunTranslationConsts.Translation.MaximumLength)
+                .WithMessage($"Maximum allowed length is {FunTranslationConsts.Translation.MaximumLength}");
+    }
+}

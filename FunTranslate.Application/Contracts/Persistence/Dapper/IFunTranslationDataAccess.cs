5a6c350d828a856765ac981d0d8d24559d549cfc
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-05-29 19:59:49 +0300

    Add Dapper Contracts to Application Layer

---
+++ FunTranslate.Application/Contracts/Persistence/Dapper/IFunTranslationDataAccess.cs
@@ -0,0 +1,5 @@
+﻿namespace FunTranslate.Application.Contracts.Persistence.Dapper;
+public interface IFunTranslationDataAccess
+{
+    Task<IEnumerable<FunTranslation>> Filter(FunTranslation filter);
+}
---
+++ FunTranslate.Application/Contracts/Persistence/Dapper/ISqlDataAccess.cs
@@ -0,0 +1,6 @@
+﻿namespace FunTranslate.Application.Contracts.Persistence.Dapper;
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters);
+    Task SaveData<T>(string storedProcedure, T parameters);
+}
